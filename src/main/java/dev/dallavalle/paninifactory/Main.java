38afc8fb4022d4ad408b45b92ba697c3dae0176e
Horacio Dalla Valle <horacio.dalla@despegar.com>
2024-01-02 05:46:55 -0300

    Add the initial code to send a WhatsApp message

--- src/main/java/dev/dallavalle/paninifactory/Main.java
+++ src/main/java/dev/dallavalle/paninifactory/Main.java
@@ -1,4 +1,35 @@
 package dev.dallavalle.paninifactory;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
+    public static void main(String[] args) {
+        sendWhatsAppMessage();
+    }
+
+    private static void sendWhatsAppMessage() {
+        try {
+            String phoneNumberId = System.getenv("PHONE_NUMBER_ID");
+            String accessToken = System.getenv("ACCESS_TOKEN");
+            String recipientNumber = System.getenv("RECIPIENT_NUMBER");
+
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(new URI(String.format("https://graph.facebook.com/v13.0/%s/messages", phoneNumberId)))
+                .header("Authorization", String.format("Bearer %s", accessToken))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(String.format("{ \"messaging_product\": \"whatsapp\", \"recipient_type\": \"individual\", \"to\": \"%s\", \"type\": \"text\", \"text\": { \"preview_url\": false, \"body\": \"This is an example of a text message\" } }", recipientNumber)))
+                .build();
+
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (URISyntaxException | IOException | InterruptedException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
---
+++ src/test/java/dev/dallavalle/paninifactory/MainTest.java
@@ -0,0 +1,24 @@
+package dev.dallavalle.paninifactory;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@Disabled
+class MainTest {
+    @Test
+    void loadImageFromDisk_ValidImage_ReturnsImage() {
+        fail();
+    }
+
+    @Test
+    void sendWhatsAppSticker_ValidImage_CompletesSuccessfully() {
+        fail();
+    }
+
+    @Test
+    void sendWhatsAppMessage_ValidMessage_CompletesSuccessfully() {
+        fail();
+    }
+}
